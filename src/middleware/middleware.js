7ef2e916cc0feab76550fe6294ee2f7530554937
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-07-30 18:11:43 -0300

    User registration

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este o valor da variável local.'
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 }
 
